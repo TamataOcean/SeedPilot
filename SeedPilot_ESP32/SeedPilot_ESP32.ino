1a5a1fb7100892635ad12c313603db19eecb4bf9
Romain Tourte <rtourte@yahoo.fr>
2024-06-18 16:24:36 +0200

    add device

--- SeedPilot_ESP32/SeedPilot_ESP32.ino
+++ SeedPilot_ESP32/SeedPilot_ESP32.ino
@@ -30,6 +30,7 @@ BME280 sensorBME280;
 #define ONE_WIRE_BUS 14
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature ds(&oneWire);
+// device external
 DeviceAddress Thermometer;
 int deviceCount = 0;
 
